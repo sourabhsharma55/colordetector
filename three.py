8cdff8b70cea88659f39053c69202b81a1eaa4a7
sourabhsharma55 <52452442+sourabhsharma55@users.noreply.github.com>
2019-08-09 13:36:13 +0530

    Add files via upload

--- three.py
+++ three.py
@@ -1,80 +1,93 @@
-import numpy as np 
-import cv2 
-import json
-from SYNVISION import colordetector
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt 
 
-#-----------------------------------------------------------------------------------------------------------------------------------------------
-def gstreamer_pipeline (capture_width=600, capture_height=450,display_width=600, display_height=450, framerate=60, flip_method=0) :   
-	return ('nvarguscamerasrc ! ' 
-	'video/x-raw(memory:NVMM), '
-	'width=(int)%d, height=(int)%d, '
-	'format=(string)NV12, framerate=(fraction)%d/1 ! '
-	'nvvidconv flip-method=%d ! '
-	'video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! '
-	'videoconvert ! '
-	'video/x-raw, format=(string)BGR ! appsink'  % (capture_width,capture_height,framerate,flip_method,display_width,display_height))
-#------------------------------------------------------------------------------------------------------------------------------------------------
-port = "5553"
-if len(sys.argv) > 1: 
-port =  sys.argv[1]
-int(port)
+img = cv2.imread('plus3.jpg')
+# img = cv2.imread('cross3.jpg')
 
-context = zmq.Context()
-socket = context.socket(zmq.PUB)
-socket.bind("tcp://*:%s" % port)
-cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
-jpeg_quality = 95
+img = cv2.medianBlur(img,5)
+img = cv2.bilateralFilter(img,9,75,75)
+img = cv2.medianBlur(img,5)
 
-# camera------------------------------------------------------------------------
-cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+plt.imshow(img)
+plt.show()
 
-# screw_class--------------------------------------------------------------------
-class Tester():
+#------------------------image 1--------------------------------------
+img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(img_gray, 127, 255,0)
+_,contours,hierarchy = cv2.findContours(thresh,2,1)
 
-	def __init__(self):
-		self.detectorObject = colordetector.detectColor()
-		#_,self.frame = cap.read()
-		with open('/home/nano2/new_qc/qc_modules/config.json') as f:
-		    configuration = json.load(f)
-		self.screwConfig = configuration['missingScrews']
-		colours = self.screwConfig['colourDict']
+cnt = contours[1]
 
-	for name in colours:
-	    lb = np.array(colours[name][0])
-	    ub = np.array(colours[name][1])
-	    self.detectorObject.clrDict[name] = [lb,ub]
+hull = cv2.convexHull(cnt,returnPoints = False)
+defects = cv2.convexityDefects(cnt,hull)
 
-	self.detectorObject.checkBoxes = self.screwConfig['boundingBox']
+print("number of defects img_1 : ",defects.shape[0])
 
-	def run(self,val):  # 'val' is the frames from the gstreamer_camera
+l1 = []
+for i in range(defects.shape[0]):
+	s,e,f,d = defects[i,0]
+	if d>200 :
+		p = d//256
+		l1.append(p)
+		start = tuple(cnt[s][0])
+		end = tuple(cnt[e][0])
+		far = tuple(cnt[f][0])
+		cv2.line(img,start,end,[0,255,0],3)
+		cv2.circle(img,far,2,[0,0,255],7)
 
-		frame  = self.detectorObject.detect(val,
-		self.screwConfig['returnFoundColour'],
-		self.screwConfig['requiredPixelRatio'],
-		self.screwConfig['reverseFlag'])
-		return frame
-'''
-obj = Tester()
-cv2.imshow('Detection',obj.run())
-cv2.waitKey(0000)
-'''
 
-obj = Tester()
-while True:
-	try:
-		ret, val = cap.read()
-	if ret:
-		ret_code, jpg_buffer = cv2.imencode(
-		".jpg", obj.run(val), [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality])
-		socket.send(jpg_buffer.tostring())
-		print "Frames sent"
-		key = cv2.waitKey(1):
-		if key == ord("c"):
-			exit(0)
+plt.imshow(img)
+plt.show()
 
-	except KeyboardInterrupt:
-		socket.close()
-		print "server is closed!"
-		break
+#-----------------image 2---------------------------------------------
+img = cv2.imread('cross.png')
 
-exit(0)
+img = cv2.medianBlur(img,5)
+img = cv2.blur(img,(2,2))
+img = cv2.medianBlur(img,5)
+
+plt.imshow(img)
+plt.show()
+
+img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(img_gray, 127, 255,0)
+_,contours,hierarchy = cv2.findContours(thresh,2,1)
+cnt = contours[1]
+
+hull = cv2.convexHull(cnt,returnPoints = False)
+defects = cv2.convexityDefects(cnt,hull)
+
+print("number of defects img_1 : ",defects.shape[0])
+print(defects[0])
+
+l2 = []
+for i in range(defects.shape[0]):
+	s,e,f,d = defects[i,0]
+	if d>200 :
+		p = d//256
+		l2.append(p)
+		start = tuple(cnt[s][0])
+		end = tuple(cnt[e][0])
+		far = tuple(cnt[f][0])
+		cv2.line(img,start,end,[0,255,0],3)
+		cv2.circle(img,far,2,[0,0,255],7)
+
+
+plt.imshow(img)
+plt.show()
+
+s = sum(l1)
+for i in range(len(l1)):
+	l1[i] = l1[i]/s
+
+s = sum(l2)
+for i in range(len(l2)):
+	l2[i] = l2[i]/s
+
+
+print("l1 : ",sorted(l1,reverse = True))
+print("l2 : ",sorted(l2,reverse=True))
+
+if l1==l2:
+	print("MATCHED")
